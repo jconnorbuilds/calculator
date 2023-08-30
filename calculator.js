9d8d3e032cdae919f581a151f7f5b9fc9b7ec755
Joe Connor <joseph.connor.music@gmail.com>
2023-08-30 18:27:49 +0900

    Get basic functions working
    Having trouble 'chaining' operations together, still not
    working intuitively yet

---
+++ calculator.js
@@ -0,0 +1,121 @@
+const calcDisplayMain = document.querySelector('.display-main');
+const calcDisplaySub = document.querySelector('.display-sub');
+const calcButtons = document.querySelectorAll('.all-buttons');
+const numberButtons = document.querySelectorAll('.calc .number');
+const operatorButtons = document.querySelectorAll('.calc .operator');
+
+const clearButton = document.querySelector('.calc .clear');
+const posNegButton = document.querySelector('.calc .posneg');
+const exponentButton = document.querySelector('.calc .power'); 
+const plusButton = document.querySelector('.btn-plus');
+const minusButton = document.querySelector('.btn-minus');
+const multiplyButton = document.querySelector('.btn-mult');
+const divideButton = document.querySelector('.btn-divide');
+const equalsButton = document.querySelector('.btn-equals');
+
+let value1 = '';
+let value2 = '';
+let operator = '';
+let result = '';
+let error = false;
+
+let symbols = {
+  add: '+',
+  subtract: '-',
+  multiply: '×',
+  divide: '÷',
+}
+
+let operatorFunctions = {
+  'add': add,
+  'subtract': subtract,
+  'multiply': multiply,
+  'divide': divide,
+}
+
+numberButtons.forEach((btn) => btn.addEventListener('click', (e) => {
+  if (error === true) initialize();
+  if (calcDisplayMain.textContent.length <= 8) {
+    if (btn.classList.contains('btn-decimal')) {
+      if (!calcDisplayMain.textContent.includes('.')) {
+        calcDisplayMain.textContent += btn.dataset.value;
+      }
+    } else {
+      calcDisplayMain.textContent += btn.dataset.value;
+    }
+  };
+}));
+
+operatorButtons.forEach((btn) => btn.addEventListener('click', (e) => {
+  value1 = parseFloat(calcDisplayMain.textContent);
+  if (value1) operator = operatorFunctions[e.target.dataset.func];
+  value2 = '';
+  result = '';
+  updateDisplay(`${value1} ${symbols[operator.name]} ${value2}`, result);
+}));
+
+clearButton.addEventListener('click', () => {
+  calcDisplayMain.textContent ? updateDisplay('', '') : initialize()
+});
+
+equalsButton.addEventListener('click', () => {
+  value2 = !result && calcDisplayMain.textContent ?
+    parseFloat(calcDisplayMain.textContent) : value2;
+  value1 = result ? result : value1;
+
+  if (operator && calcDisplayMain.textContent) operate(value1, value2, operator);
+
+});
+
+function initialize() {
+  updateDisplay()
+  value1 = '';
+  value2 = '';
+  operator = '';
+  result = '';
+};
+
+function updateDisplay(sub, main) {
+  calcDisplaySub.textContent = sub;
+  calcDisplayMain.textContent = main;;
+}
+
+function round(int, places=6) {
+  return Math.round(int * (10 ** places))/(10 ** places)
+}
+
+
+function add(a, b) {
+  return a + b;
+};
+
+function subtract(a, b) {
+  return a - b;
+};
+
+function multiply(a, b) {
+  return a * b;
+};
+
+function divide(a, b) {
+  if (b != 0) {
+    return a / b;
+  } else {
+    updateDisplay(`>:(`,"...");
+  };
+};
+
+function operate(val1, val2, operator) {
+  result = round(operator(val1, val2));
+  result = result.toString().length > 9 ? result.toExponential(5) : result;
+  
+  if (!isNaN(result)) {
+    updateDisplay(`${val1} ${symbols[operator.name]} ${val2}`, result);
+  } else if (isNaN(result)) {
+    error = true;
+    result = '';
+    value1 = '';
+    value2 = '';
+    operator = '';
+  };
+}
